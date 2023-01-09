4025be7f1fa6e71b6bb39ecb6bd99816f324c6a3
eycha <silvlexonst@gmail.com>
2023-01-09 12:13:37 +0900

    bulid: to-do input adding

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
   <div id="app" class="container">
     <h1 class="text-center">Todo App</h1>
-    <input type="text" class="w-100 p-2" placeholder="Type todo" />
+    <input
+      v-model="todoText"
+      type="text"
+      class="w-100 p-2"
+      placeholder="Type todo"
+      @keyup.enter="addTodo"
+    />
     <hr />
-    <Todo v-for="todo in todos" :key="todo.id" />
+    <Todo v-for="todo in todos" :key="todo.id" :todo="todo" />
   </div>
 </template>
 
@@ -15,11 +21,23 @@ export default {
   },
   data() {
     return {
+      // todoText: "",
       todos: [
         {id: 1, text: "buy a car", checked: false},
-        {id: 1, text: "buy a car", checked: false},
+        {id: 2, text: "play game", checked: false},
       ],
     };
+  },
+
+  methods: {
+    addTodo(e) {
+      this.todos.push({
+        id: Math.random(),
+        text: e.target.value,
+        checked: false,
+      });
+      this.todoText = "";
+    },
   },
 };
 </script>
--- src/components/Todo-comp.vue
+++ src/components/Todo-comp.vue
@@ -1,16 +1,22 @@
 <template>
   <div>
-    <input type="checkbox" />
-    <span class="ml-3">Buy a car</span>
+    <input type="checkbox" :checked="todo.checked" />
+    <span class="ml-3">{{ todo.text }}</span>
   </div>
 </template>
 
 <script>
 export default {
-  beforeMount() {},
-  mounted() {},
-  beforeUnmount() {},
-  unmounted() {},
+  // beforeMount() {},
+  // mounted() {},
+  // beforeUnmount() {},
+  // unmounted() {},
+  props: {
+    todo: {
+      type: Object,
+      required: true,
+    },
+  },
 };
 </script>
 
